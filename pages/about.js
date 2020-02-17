2a5cee4453200dc382393a522526ec6f627aaf2a
maksimgm <maksimmamrikov@gmail.com>
2020-02-16 18:14:10 -0800

    add navbar to components

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+import { NavBar } from '../components/NavBar';
+
+const About = () => (
+  <div>
+    <NavBar/>
+    <h1>About</h1>
+    <p>Application to view BTC prices</p>
+  </div>
+)
+export default About
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+import { NavBar } from '../components/NavBar';
+
+const Index = () => (
+  <div>
+    <NavBar />
+    <h1>Hello Crypto Pricer</h1>
+  </div>
+)
+export default Index
